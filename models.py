df67e5853beb8c16cb1578074fa0c102e1c03e1a
ollie <olivinanzali07@gmail.com>
2022-08-24 20:42:35 -0400

    updates to models.py

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 def connect_db(app):
@@ -11,12 +13,13 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    username = db.Column(db.String(20), 
+    username = db.Column(db.String(20),
+                        nullable=False, 
                         unique=True, 
                         primary_key=True)
     password = db.Column(db.Text, 
                         nullable=False)
-    email = db.Column(db.Email(50), 
+    email = db.Column(db.String(50), 
                      unique=True, 
                      nullable=False)
     first_name = db.Column(db.String(30),
@@ -24,12 +27,42 @@ class User(db.Model):
     last_name = db.Column(db.String(30),
                           nullable=False)
 
+@classmethod
+def register(cls, username, password, first_name, last_name, email):
+
+    """registering user and hashing password"""
+
+    hashed = bcrypt.generate_password_hash(password)
+    hashed_utf8 = hashed.decode("utf8")
+    user = cls(
+        username=username,
+        password=hashed_utf8,
+        first_name=first_name,
+        last_name=last_name,
+        email=email
+    )
+
+    db.session.add(user)
+    return user
+
+@classmethod
+def authenticate(cls, username, password):
+    """ verifying user is username and password is right"""
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        return user
+    else:
+        return False
+
+
 class Feedback(db.Model):
 
-    __tablename__ = "feedbacks"
+    __tablename__ = "feedback"
 
     id = db.Column(db.Integer, unique=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullble=False)
-    username = db.Column(db.ForeignKey('users.username'))
+    username = db.Column(db.String(20), db.ForeignKey('users.username'))
 
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ollie/Documents/Flask-Feedback/venv"
+set -gx VIRTUAL_ENV "/Users/ollie/Documents/Feedback_Flask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
